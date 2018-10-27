1d50a03fb01539d7192ac2ab42dd6061e413daab
Carlos <carlosbrunotbc@gmail.com>
2018-10-27 04:53:36 -0300

    fest(index): to integrate server with express

---
+++ app.js
@@ -0,0 +1,25 @@
+const api = require('./routes');
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use('/api', api);
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+const PORT = process.env.PORT || 8000;
+
+const start = () => {
+    app.listen(PORT, () => {
+        console.log(`Server running at port: ${PORT}`);
+    })
+}
+
+start();
